362593be9c3920e70a3c8d046ea5f61636f6dd36
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-10-18 11:21:03 +0900

    devise/model controllers views導入 root設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'homes#index'
+  devise_for :users, :controllers => {
+    :sessions      => "users/sessions",
+    :registrations => "users/registrations",
+    :passwords     => "users/passwords" 
+  }
+  resources :users, :only => [:index, :show]
+  # devise_for :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
