8c5c5f8ee789dbb86a5e8ddba8374382ac1e99a1
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-10-19 17:55:39 +0900

    ユーザ一覧ページにページネーションを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+    @users = User.order(:created_at, :id).page(params[:page]).per(3)
   end
 
   def show
