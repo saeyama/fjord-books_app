62d93cb085690a51546e124fb8f2130ec0bc4668
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-10-20 00:25:28 +0900

    kaminari日本語設定・rubocop通過確認

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :encrypted_password, :name, :postal_code, :prefecture_code, :city, :other_address, :profile ])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [ :name, :encrypted_password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email encrypted_password name postal_code prefecture_code city other_address profile])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[name encrypted_password])
   end
 end
--- db/migrate/20211017143924_devise_create_users.rb
+++ db/migrate/20211017143924_devise_create_users.rb
@@ -11,8 +11,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :street
       t.string :other_address
       t.text :profile
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
       t.string   :reset_password_token
@@ -38,8 +38,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
-
       t.timestamps null: false
     end
 
