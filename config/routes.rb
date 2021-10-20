56f1aec85952fc8850cdccb400db147dd1d1d855
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-10-20 20:58:01 +0900

    パスワード再設定メールを日本語表記に変更・rubocop通過確認。

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :sessions      => "users/sessions",
     :registrations => "users/registrations",
-    :passwords     => "users/passwords" 
+    :passwords     => "users/passwords",
+    :mailer        => "users/mailer"
   }
   
   devise_scope :user do
