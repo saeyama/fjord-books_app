3b0ca52a7d1c11e2d4c3a311448dae777e61edbf
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-10-23 01:16:03 +0900

    画像アップロード機能を実装・日本語表記に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
+  validate :avatar_extension
+
+  def avatar_extension
+    ['avatar/png', 'avatar/jpg', 'avatar/jpeg']
+  end       
 end
