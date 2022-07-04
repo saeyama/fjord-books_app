6cc641825e8cec1552fad17bdd0caef6f405db72
saeyama <camper_green_yellow_21@yahoo.co.jp>
2022-07-04 21:25:35 +0900

    from_omniauthで使用していたfirst_or_createをfind_or_create_byに修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :uid, presence: true, uniqueness: { scope: :provider }
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
